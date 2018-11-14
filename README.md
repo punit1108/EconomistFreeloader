eccc8f092baf8d4a5fad54a3fc9f1728a64b98cb
punit1108 <punitkpal@gmail.com>
2018-11-14 18:58:43 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+The Economist Freeloader
+========================
+
+## Installation
+
+It's pretty easy to install extension from this sources:
+
+1. Download ZIP archive (button on the right panel) and unpack it to any convenient folder
+2. Open Chrome -> Menu - > Tools -> Extensions
+3. Check Developer mode checkbox
+4. Press Load unpacked extension button and select folder with this files
+5. Check if it is enabled and that's all!
+
+## WARNING
+
+This extension must be used only for educational purposes. Please subscribe to The Economist if it is being used elsewhere.
---
+++ script.js
@@ -0,0 +1,38 @@
+(function main() {
+
+    scrapper()
+    document.addEventListener("DOMContentLoaded", function () {
+        var blog_links = document.getElementsByClassName("teaser__link")
+
+        for(var i = 0; i< blog_links.length; i++) {
+            blog_links[i].addEventListener("click", function() {
+                scrapper()
+            })
+        }
+    })
+}())
+
+function scrapper() {
+    try {
+        var blog_content = document.querySelectorAll(".blog-post__text p")
+        var blog_title = document.querySelector(".main-content__blog-post .flytitle-and-title__title").textContent
+        var blog_tagline = document.querySelector(".main-content__blog-post .blog-post__rubric").textContent
+    } catch (e) {
+        console.log(e)
+        return;
+    }
+    console.log(blog_title)
+
+    var tab_html = "<h1>" + blog_title + "</h1><br><h3>" + blog_tagline + "</h3><br><br><br>"
+
+    blog_content.forEach(node => {
+        console.log(blog_content.innerHTML)
+        tab_html += node.outerHTML
+    })
+
+    var article = window.open()
+    article.document.write(tab_html)
+    // article.document.addEventListener("DOMContentLoaded", function() {
+        article.document.title = blog_title
+    // })
+}
